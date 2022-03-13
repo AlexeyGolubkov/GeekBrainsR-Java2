c56f1e3f279f0b86382b56b776b7fa06516d0e3f
Alexey Golubkov <mannallgoall@gmail.com>
2022-03-13 18:47:56 +0400

    Lesson7_add_loss_directory

---
+++ part1Lesson7/src/main/java/Forecast.java
@@ -0,0 +1,19 @@
+public class Forecast {
+    //Forecastday[] forecastday = new Forecastday[1];
+    Forecastday[] forecastday;
+
+    // Getter Methods
+
+    public Forecastday[] getForecastday() {
+        return forecastday;
+    }
+
+    public void setForecastday(Forecastday[] forecastday) {
+        this.forecastday = forecastday;
+    }
+
+
+    // Setter Methods
+
+
+}
